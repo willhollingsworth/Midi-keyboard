4453e9b43d05a48405ac9bd01720ecd3423f0b53
Will Hollingsworth <69777654+willhollingsworth@users.noreply.github.com>
2022-08-10 09:52:28 +0930

    added major scales

--- Midi Keyboard.py
+++ Midi Keyboard.py
@@ -1,3 +1,4 @@
+from operator import index
 import pynput
 import rtmidi
 import keyboard
@@ -20,7 +21,7 @@ def send_midi(note, state='up'):
 
 
 def key_down(key):
-    global starting_note
+    global starting_octeve
     if active_keys:
         if str(key) in active_keys:
             return
@@ -28,10 +29,11 @@ def key_down(key):
     if str(key) in utility_hotkeys:
         if str(key) == 'Key.up':
             release_all_held_keys()
-            starting_note += 1
+            starting_octeve += 1
         if str(key) == 'Key.down':
             release_all_held_keys()
-            starting_note -= 1
+            if starting_octeve > 0:
+                starting_octeve -= 1
     active_keys.append(str(key))
 
 
@@ -40,6 +42,20 @@ def key_up(key):
     if active_keys:
         if str(key) in active_keys:
             active_keys.remove(str(key))
+
+
+def calc_note_offset(key):
+    offset = scale_offset
+    scale_target = active_scale
+    main_index = note_hotkeys.index(key.char)
+    scale_length = len(scale_target)
+    scale_index, note_index = divmod(main_index, scale_length)
+    if scale_index != 0:
+        offset += scale_index*sum(scale_target)
+    if note_index != 0:
+        offset += sum(scale_target[0:note_index])
+    print(main_index, scale_index, note_index, offset)
+    return offset
 
 
 def key_event(key, state):
@@ -51,7 +67,7 @@ def key_event(key, state):
     if not 'char' in dir(key):
         return
     if key.char in note_hotkeys:
-        note = starting_note + note_hotkeys.index(key.char)
+        note = starting_octeve*8 + calc_note_offset(key)
         send_midi(note, state)
     return
 
@@ -76,17 +92,22 @@ def disable_regular_hotkey_usage():
 
 
 global note_hotkeys
-global starting_note
+global starting_octeve
 global active_keys
 global utility_hotkeys
 
 note_hotkeys = '1234567890qwertyuiopasdfghjklzxcvbnm'
 utility_hotkeys = ['Key.up', 'Key.down']
 # utility_hotkeys = ['Key.' + key for key in utility_hotkeys]
-starting_note = 50
+starting_octeve = 0
 active_keys = []
-major_scale = [2, 2, 1, 2, 2, 2, 1]
-
+major_scale_offsets = [2, 2, 1, 2, 2, 2, 1]
+major_scale_root_list = ['c', 'c#', 'd', 'd#',
+                         'e', 'e#', 'f',  'g', 'g#',  'a', 'a#', 'b']
+root_note = 'a#'
+active_scale = major_scale_offsets
+scale_offset = major_scale_root_list.index(root_note)
+print('scale offset', scale_offset)
 midiout = setup_midi_port()
 disable_regular_hotkey_usage()
 start_keyboard_listening()
